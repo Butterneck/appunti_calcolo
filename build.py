09b004f1c4d5464588680022b356f1adede13dfa
Filippo <pinton.filippo@protonmail.com>
2020-04-04 21:58:22 +0200

    Updated build script to include images in pdfs

--- build.py
+++ build.py
@@ -4,39 +4,43 @@ from os import listdir, getcwd, mkdir, remove
 from os.path import isdir, isfile, join, splitext
 import subprocess
 from subprocess import check_output
-
-
-def build():
-    dirs = getDirs()
-
-    mkdir('output')
-
-    for dir in dirs:
-        basename = "lezione" + str(int(dir[:2])) 
-        jobname = "-jobname=" + "/".join(["output", basename])
-        lezione = "/".join([dir, basename]) + ".tex"
-
-        buildCmd = ["docker", "run", "-i", "-v", getcwd() + ":/data", "blang/latex", "latexmk", "-interaction=nonstopmode", "-pdf", jobname, lezione]
-
-        subprocess.run(buildCmd)
-        clearOutputDir()
+from shutil import move
 
 
 def getDirs(path='.'):
     return [f for f in listdir(path) if (isdir(join(path, f)) and not (f == '.git'))]
 
 
-def clearOutputDir(path = './output'):
-    files = [f for f in listdir(path) if isfile(join(path, f))]
+def buildDirs(dirs):
+    for dir in dirs:
+        buildDir(dir)
 
-    for file in files:
-        filename, file_extension = splitext(file)
-        if file_extension != '.pdf':
-            remove('./output/' + filename + file_extension)
+
+def buildDir(dir):
+    basename = "lezione" + str(int(dir[:2]))
+    jobname = "-jobname=" + basename
+    lezione = ".".join([basename, "tex"])
+
+    buildCmd = ["docker", "run", "-i", "-v", "/".join([getcwd(), dir]) + ":/data", "blang/latex", "latexmk", "-interaction=nonstopmode", "-pdf", jobname, lezione]
+    subprocess.run(buildCmd)
+
+
+def groupPdfs(dirs, outputDir='./output'):
+    mkdir(outputDir)
+
+    for dir in dirs:
+        path = "/".join([getcwd(), dir])
+        files = [f for f in listdir(path) if isfile(join(path, f))]
+        for file in files:
+            filename, file_extension = splitext(file)
+            if file_extension == '.pdf':
+                move("/".join([path, filename + file_extension]), "/".join([outputDir, filename + file_extension]))
 
 
 def main():
-    build()
+    dirs = getDirs()
+    buildDirs(dirs)
+    groupPdfs(dirs)
 
 
 if __name__ == '__main__':
