f2a86c2f58207cbb1bed7f5ec04be5f8d65545ce
Filippo <pinton.filippo@protonmail.com>
2020-04-04 19:58:03 +0200

    Added build script

---
+++ build.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from os import listdir, getcwd, mkdir, remove
+from os.path import isdir, isfile, join, splitext
+import subprocess
+from subprocess import check_output
+
+
+def buildDirs(dirs):
+    dirs = getDirs()
+
+    mkdir('output')
+
+    for dir in dirs:
+        basename = "lezione" + str(int(dir[:2])) 
+        jobname = "-jobname=" + "/".join(["output", basename])
+        lezione = "/".join([dir, basename]) + ".tex"
+
+        buildCmd = ["docker", "run", "-i", "-v", getcwd() + ":/data", "blang/latex", "latexmk", "-interaction=nonstopmode", "-pdf", jobname, lezione]
+
+        subprocess.run(buildCmd)
+        clearOutputDir()
+
+
+def getDirs(path='.'):
+    return [f for f in listdir(path) if (isdir(join(path, f)) and not (f == '.git'))]
+
+
+def clearOutputDir(path = './output'):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+
+    for file in files:
+        filename, file_extension = splitext(file)
+        if file_extension != '.pdf':
+            remove('./output/' + filename + file_extension)
+
+
+def main():
+    buildDirs(dirs)
+
+
+if __name__ == '__main__':
+    main()
